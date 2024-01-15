b9c027f12a2d4657e0ac2957f77ed663e1b29aa9
Breno.gomes <breno.gomes@totvs.com.br>
2024-01-15 17:53:43 -0300

    ajuste na estrutura de pasta

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PoModule } from '@po-ui/ng-components';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { MotoristaComponent } from './cadastros/motorista/motorista.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MotoristaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PoModule,
+    HttpClientModule,
+    RouterModule.forRoot([])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
