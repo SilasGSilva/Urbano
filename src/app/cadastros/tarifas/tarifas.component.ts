bf6632eb682ca9e1c5bfd61096840236bbbfa87a
Levy Meireles Rodrigues dos Santos <levymeireles518@gmail.com>
2024-01-31 18:23:34 -0300

    init page tarifas

---
+++ src/app/cadastros/tarifas/tarifas.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { PoBreadcrumb, PoPageAction } from '@po-ui/ng-components';
+
+@Component({
+  selector: 'app-tarifas',
+  templateUrl: './tarifas.component.html',
+  styleUrls: ['./tarifas.component.css'],
+})
+export class TarifasComponent {
+  constructor() {}
+
+  actions: Array<PoPageAction> = [
+    {
+      label: 'Incluir',
+      action: () => {
+        this.addTariff();
+      },
+    },
+  ];
+
+  public breadcrumb: PoBreadcrumb = {
+    items: [
+      { label: 'Fretamento Urbano', link: '/' },
+      { label: 'Localidades' },
+    ],
+  };
+
+  addTariff() {}
+}
